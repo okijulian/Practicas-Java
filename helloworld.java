f8022e94bfadad3218330fab818c654f8a3b7218
Oscar J <okijulian0791@gmail.com>
2021-09-29 19:04:15 -0300

    primer archivo en java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println(" hola mundo desde java");
+        System.out.println(" hola mundo ");
+    }
+}
