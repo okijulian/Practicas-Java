c3cd0627ad243ae08558340ca9da2d906e9bb010
Oscar J <okijulian0791@gmail.com>
2021-10-04 19:20:08 -0300

    nuevo commit

---
+++ Concatenaciones.java
@@ -0,0 +1,28 @@
+public class Concatenaciones {
+    
+   public static void main(String[] args) {
+    //concatenacion de strings.
+    var user="okisan";
+    var title="developer";
+    var union =  title + " "  + user; 
+    System.out.println("union= " +  union);
+
+    //concatenacion de numeros. 
+
+    var i=10;
+    var j=5;
+    var suma= i+j;
+    var resta= i-j;
+    var multiplicacion= i*j;
+    var division =  i/j;
+
+    System.out.println(suma);
+    System.out.println(resta);
+    System.out.println(multiplicacion);
+    System.out.println(division);
+
+        
+    }
+
+
+}
---
+++ Tipovariables.java
@@ -0,0 +1,27 @@
+public class Tipovariables {
+  
+    public static void main(String[] args) {
+
+        // tipos enteros
+        int miVariable = 10;
+        System.out.println(miVariable);
+        // modificando el valor de la variable
+        miVariable = 5;
+        System.out.println(miVariable);
+
+        
+        // tipo cadenas o strings
+        String miString="hola desde java";         
+        System.out.println(miString);
+
+        // var -- inferencia de tipos en java
+        var miVariableVar=230;
+        System.out.println(miVariableVar);
+
+        var miVariableVarString="variable string";
+        System.out.println(miVariableVarString);
+
+
+    }
+    
+}
